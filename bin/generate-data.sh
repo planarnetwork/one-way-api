bf9a91f3d0c460eece30610916c3c113ba27fe5f
Linus Norton <linusnorton@gmail.com>
2019-05-15 21:11:57 +0100

    more data

--- bin/generate-data.sh
+++ bin/generate-data.sh
@@ -10,15 +10,11 @@ wget -P -N https://transitfeeds.com/p/renfe/1018/latest/download -O /tmp/es-inte
 wget -P -N https://transitfeeds.com/p/renfe/1016/latest/download -O /tmp/es-catelonia-all.zip
 wget -P -N http://data.ndovloket.nl/flixbus/flixbus-eu.zip -O /tmp/eu-flix-bus.zip
 wget -P -N https://api.idbus.com/gtfs.zip -O /tmp/eu-oui-bus.zip
+wget -P -N https://github.com/fredlockheed/db-fv-gtfs/releases/download/2019.0.0/2019.zip -O /tmp/de-intercity-rail.zip
+wget -P -N https://gtfs.geops.ch/dl/gtfs_train.zip -O /tmp/ch-rail.zip
 
 # DB, lots of data but only covers a small area?
 # wget -P -N https://data.opendatasoft.com/explore/dataset/de@navitia/files/400d7da94eaacb5e52c612f8ac28e420/download/ -O /tmp/de-rail.zip
-
-# DB, too many calendar dates
-# wget -P -N https://github.com/fredlockheed/db-fv-gtfs/releases/download/2019.0.0/2019.zip -O /tmp/de-intercity-rail.zip
-
-# Swizterland, Too many calendar dates
-# wget -P -N https://gtfs.geops.ch/dl/gtfs_train.zip -O /tmp/ch-rail.zip
 
 # NL, Too much data?
 # wget -P -N http://gtfs.ovapi.nl/nl/gtfs-nl.zip -O /tmp/nl-all.zip
@@ -37,5 +33,7 @@ wget -P -N https://api.idbus.com/gtfs.zip -O /tmp/eu-oui-bus.zip
   /tmp/eu-oui-bus-modified.zip \
   /tmp/es-intercity-rail.zip \
   /tmp/es-catelonia-all.zip \
+  /tmp/de-intercity-rail.zip \
+  /tmp/ch-rail.zip \
   /tmp/fr-all.zip \
   ./data/output.zip
