0e8440d2bad2ed56878103afaed1b4556697c6c1
Linus Norton <linusnorton@gmail.com>
2019-06-04 21:10:28 +0100

    update data

--- bin/generate-data.sh
+++ bin/generate-data.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env sh
-wget -P -N https://s3.eu-west-2.amazonaws.com/feeds.planar.network/gb-rail-latest.zip -O /tmp/gb-rail.zip
-wget -P -N https://s3.eu-west-2.amazonaws.com/feeds.planar.network/ncsd-bus-latest.zip -O /tmp/gb-coach.zip
+mkdir -p /tmp/oneway
+
+wget -P -N https://s3.eu-west-2.amazonaws.com/feeds.planar.network/gb-rail-latest.zip -O /tmp/oneway/gb-rail.zip
+wget -P -N https://s3.eu-west-2.amazonaws.com/feeds.planar.network/ncsd-bus-latest.zip -O /tmp/oneway/gb-coach.zip
 wget -P -N https://data.sncf.com/explore/dataset/sncf-intercites-gtfs/files/ed829c967a0da1252f02baaf684db32c/download/ -O /tmp/fr-intercity-rail.zip
 wget -P -N https://data.sncf.com/explore/dataset/sncf-ter-gtfs/files/24e02fa969496e2caa5863a365c66ec2/download/ -O /tmp/fr-ter-rail.zip
 wget -P -N https://s3.eu-west-2.amazonaws.com/feeds.planar.network/eurostar.zip -O /tmp/eurostar.zip
-wget -P -N https://gtfs.irail.be/nmbs/gtfs/gtfs-nmbs-2019-03-12.zip -O /tmp/be-intercity-rail.zip
-wget -P -N https://transitfeeds.com/p/openov/621/latest/download -O /tmp/lu-intercity-rail.zip
-wget -P -N https://transitfeeds.com/p/renfe/1018/latest/download -O /tmp/es-intercity-rail.zip
-wget -P -N http://data.ndovloket.nl/flixbus/flixbus-eu.zip -O /tmp/eu-flix-bus.zip
+wget -P -N https://gtfs.irail.be/nmbs/gtfs/gtfs-nmbs-2019-03-12.zip -O /tmp/oneway/be-intercity-rail.zip
+wget -P -N https://transitfeeds.com/p/openov/621/latest/download -O /tmp/oneway/lu-intercity-rail.zip
+wget -P -N https://transitfeeds.com/p/renfe/1018/latest/download -O /tmp/oneway/es-intercity-rail.zip
+wget -P -N http://data.ndovloket.nl/flixbus/flixbus-eu.zip -O /tmp/oneway/eu-flix-bus.zip
 wget -P -N https://api.idbus.com/gtfs.zip -O /tmp/eu-oui-bus.zip
-wget -P -N https://github.com/fredlockheed/db-fv-gtfs/releases/download/2019.0.0/2019.zip -O /tmp/de-intercity-rail.zip
-wget -P -N https://gtfs.geops.ch/dl/gtfs_train.zip -O /tmp/ch-rail.zip
+wget -P -N https://github.com/fredlockheed/db-fv-gtfs/releases/download/2019.0.0/2019.zip -O /tmp/oneway/de-intercity-rail.zip
+wget -P -N https://gtfs.geops.ch/dl/gtfs_train.zip -O /tmp/oneway/ch-rail.zip
+wget -P -N https://transitfeeds.com/p/trafiklab/50/latest/download -O /tmp/oneway/se-all.zip
+wget -P -N https://transitfeeds.com/p/entur/970/latest/download -O /tmp/oneway/no-all.zip
+wget -P -N https://transitfeeds.com/p/ministry-of-transport-and-communications/921/latest/download -O /tmp/oneway/no-all.zip
+wget -P -N https://transitfeeds.com/p/rejseplanen/705/latest/download -O /tmp/oneway/dk-all.zip
 
 # DB, lots of data but only covers a small area?
 # wget -P -N https://data.opendatasoft.com/explore/dataset/de@navitia/files/400d7da94eaacb5e52c612f8ac28e420/download/ -O /tmp/de-rail.zip
@@ -24,17 +30,6 @@ wget -P -N https://gtfs.geops.ch/dl/gtfs_train.zip -O /tmp/ch-rail.zip
 # Not updated
 # wget -P -N https://transitfeeds.com/p/renfe/1016/latest/download -O /tmp/es-catelonia-all.zip
 
-./node_modules/.bin/gtfsmerge --transfer-distance=0.06 /tmp/fr-ter-rail.zip /tmp/fr-intercity-rail.zip /tmp/eurostar.zip /tmp/fr-all.zip
-./node_modules/.bin/gtfsmerge --stop-prefix=oui_ /tmp/eu-oui-bus.zip /tmp/eu-oui-bus-modified.zip
-./node_modules/.bin/gtfsmerge \
-  /tmp/gb-rail.zip \
-  /tmp/gb-coach.zip \
-  /tmp/be-intercity-rail.zip \
-  /tmp/lu-intercity-rail.zip \
-  /tmp/eu-flix-bus.zip \
-  /tmp/eu-oui-bus-modified.zip \
-  /tmp/es-intercity-rail.zip \
-  /tmp/de-intercity-rail.zip \
-  /tmp/ch-rail.zip \
-  /tmp/fr-all.zip \
-  ./data/output.zip
+./node_modules/.bin/gtfsmerge --transfer-distance=0.06 /tmp/fr-ter-rail.zip /tmp/fr-intercity-rail.zip /tmp/eurostar.zip /tmp/oneway/fr-all.zip
+./node_modules/.bin/gtfsmerge --stop-prefix=oui_ /tmp/eu-oui-bus.zip /tmp/oneway/eu-oui-bus-modified.zip
+./node_modules/.bin/gtfsmerge /tmp/oneway/*.zip ./data/output.zip
